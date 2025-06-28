60167b2090e3ccba6db6eab1bf19590fe6c4ee33
Ranson <ransontouch@ucla.edu>
2025-06-28 16:05:39 -0700

    Creatingfsdaf

--- assignment_1.py
+++ assignment_1.py
@@ -1,1 +1,10 @@
-#Yes, I'm doing it asdfasdf
+#Name: Ranson Touch
+#OSU Email: touchr@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment 1 - Python Fundamentals
+# Due Date: 6/8/2025
+# Description: 10 separate problems must be solved in Python in order to ensure that Python fundamentals are learned.
+
+def min_max():
+    """A function that receives a one-dimensional array and returns a Python Tuple with 2 values - the min and max values"""
+    for
