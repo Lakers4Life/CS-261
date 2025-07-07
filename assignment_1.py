99bd4daae8d9a5b98a2708e4942fcb14d3786f18
Ranson <ransontouch@ucla.edu>
2025-07-07 13:36:36 -0700

    update

--- assignment_1.py
+++ assignment_1.py
@@ -5,10 +5,23 @@
 # Due Date: 6/8/2025
 # Description: 10 separate problems must be solved in Python in order to ensure that Python fundamentals are learned.
 
-def min_max():
+import random
+from static_array import *
+
+# ------------------- PROBLEM 1 - MIN_MAX -----------------------------------
+def min_max(arr: StaticArray) -> tuple[int, int]:
     """A function that receives a one-dimensional array and returns a Python Tuple with 2 values - the min and max values"""
-    numbers += 0
-    for numbers in min_max():
+    min_value = arr.get(0)
+    max_value = arr.get(0)
+
+    for i in range(1, length(arr)):
+        value = arr.get(i)
+        if value < min_value:
+            min_value = value
+        if value > max_value:
+            max_value = value
+
+    return (min_value, max_value)
 
 
 
