edc3e1f540129e3097faf8d6be1516ddd4f96e68
Ranson <ransontouch@ucla.edu>
2025-07-07 21:03:30 -0700

    update again

---
+++ assignment1.py
@@ -0,0 +1,44 @@
+#Name: Ranson Touch
+#OSU Email: touchr@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment 1 - Python Fundamentals
+# Due Date: 6/8/2025
+# Description: 10 separate problems must be solved in Python in order to ensure that Python fundamentals are learned.
+
+import random
+from unittest import result
+
+from static_array import StaticArray
+
+# ------------------- PROBLEM 1 - MIN_MAX -----------------------------------
+def min_max(arr: StaticArray) -> tuple[int, int]:
+    """A function that receives a one-dimensional array and returns a Python Tuple with 2 values - the min and max values"""
+    min_value = arr.get(0)
+    max_value = arr.get(0)
+
+    for i in range(1, arr.length()):
+        value = arr.get(i)
+        if value < min_value:
+            min_value = value
+        if value > max_value:
+            max_value = value
+
+    return min_value, max_value
+
+# ------------------- PROBLEM 2 - FIZZ_BUZZ ---------------------------------
+def fizz_buzz(arr: StaticArray) -> StaticArray:
+    """A function that receives a StaticArray of integers and returns a new StaticArray object with the content of the original array """
+
+    for i in range(0, arr.length()):
+        value = arr.get(i)
+
+        if value % 3 and value % 5 == 0:
+            value = "fizzbuzz"
+        elif value % 5 == 0:
+            value = "buzz"
+        elif value % 3 == 0:
+            value = "fizz"
+
+    result.set(i, value)
+    return result
+
